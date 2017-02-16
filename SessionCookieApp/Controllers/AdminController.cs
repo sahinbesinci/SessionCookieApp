0b20cfd69c1f2b39198b9de78c15c99c0cec790b
Sahin <sahinbesinci@gmail.com>
2017-02-17 02:04:54 +0300

    Add project files.

---
+++ SessionCookieApp/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using SessionCookieApp.DAL;
+using SessionCookieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionCookieApp.Controllers
+{
+    [_SessionControl]
+    public class AdminController : Controller
+    {
+        authenticationuserwithoutroleContext db = new authenticationuserwithoutroleContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.users.ToList());
+        }
+    }
+}
---
+++ SessionCookieApp/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using SessionCookieApp.DAL;
+using SessionCookieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SessionCookieApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        authenticationuserwithoutroleContext db = new authenticationuserwithoutroleContext();
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (String.IsNullOrEmpty(HttpContext.User.Identity.Name))
+            {
+                FormsAuthentication.SignOut();
+                return View();
+            }
+            return Redirect("/Admin/Index");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginModel model, string returnurl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.users.Any(user => (user.Email == model.EMail && user.password == model.Password)))
+                {
+                    FormsAuthentication.SetAuthCookie(model.EMail, true);
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "EMail veya şifre hatalı!");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
